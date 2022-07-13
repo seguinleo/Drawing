23e25d7eb7fbf2d0777d64244888cea231ea5f59
PouletEnSlip <97188999+PouletEnSlip@users.noreply.github.com>
2022-07-13 18:38:25 +0200

    sources

---
+++ src/Drawing/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Drawing_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pb_canvas = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_color = new System.Windows.Forms.TextBox();
+            this.copyright = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_canvas)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pb_canvas
+            // 
+            this.pb_canvas.BackColor = System.Drawing.Color.White;
+            this.pb_canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb_canvas.Location = new System.Drawing.Point(21, 45);
+            this.pb_canvas.Name = "pb_canvas";
+            this.pb_canvas.Size = new System.Drawing.Size(576, 324);
+            this.pb_canvas.TabIndex = 0;
+            this.pb_canvas.TabStop = false;
+            this.pb_canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pb_canvas_MouseDown);
+            this.pb_canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pb_canvas_MouseMove);
+            this.pb_canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pb_canvas_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(638, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Size";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "4",
+            "6",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "14",
+            "16",
+            "18",
+            "20",
+            "22",
+            "24",
+            "28",
+            "36",
+            "48",
+            "72",
+            "96"});
+            this.comboBox1.Location = new System.Drawing.Point(686, 271);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(73, 25);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(638, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 52);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_color
+            // 
+            this.txt_color.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txt_color.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_color.Enabled = false;
+            this.txt_color.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txt_color.Location = new System.Drawing.Point(638, 245);
+            this.txt_color.Name = "txt_color";
+            this.txt_color.ReadOnly = true;
+            this.txt_color.Size = new System.Drawing.Size(121, 20);
+            this.txt_color.TabIndex = 5;
+            this.txt_color.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // copyright
+            // 
+            this.copyright.AutoSize = true;
+            this.copyright.Location = new System.Drawing.Point(21, 386);
+            this.copyright.Name = "copyright";
+            this.copyright.Size = new System.Drawing.Size(154, 20);
+            this.copyright.TabIndex = 7;
+            this.copyright.Text = "© PouletEnSlip - 2022";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(623, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 28);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Pencil settings:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(801, 28);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAsToolStripMenuItem,
+            this.clearToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.saveAsToolStripMenuItem.Text = "Save as...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(801, 415);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.copyright);
+            this.Controls.Add(this.txt_color);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb_canvas);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(819, 462);
+            this.MinimumSize = new System.Drawing.Size(819, 462);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Drawing App";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_canvas)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_canvas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_color;
+        private System.Windows.Forms.Label copyright;
+        private System.Windows.Forms.Label label2;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem saveAsToolStripMenuItem;
+        private ToolStripMenuItem clearToolStripMenuItem;
+        private ToolStripMenuItem quitToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ src/Drawing/Form1.cs
@@ -0,0 +1,129 @@
+﻿namespace Drawing_App
+{
+    public partial class Form1 : Form
+    {
+        public Point end = new Point();
+        public Point start = new Point();
+        public Pen p;
+        bool draw = false;
+        string color;
+        Graphics graphics;
+        Bitmap bmp;
+
+#pragma warning disable CS8618
+        public Form1()
+#pragma warning restore CS8618
+
+        {
+            InitializeComponent();
+            GenerateVaribles();
+        }
+
+        void GenerateVaribles()
+        {
+            graphics = pb_canvas.CreateGraphics();
+            comboBox1.Text = "10";
+            color = "#000000";
+            txt_color.Text = color;
+            CreateCanvas();
+        }
+
+        void CreateCanvas()
+        {
+            bmp = new Bitmap(pb_canvas.Width, pb_canvas.Height);
+            graphics = Graphics.FromImage(bmp);
+            pb_canvas.BackgroundImage = bmp;
+            pb_canvas.BackgroundImageLayout = ImageLayout.None;
+        }
+
+        private void pb_canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            draw = true;
+            start = e.Location;
+            int size;
+
+            if (comboBox1.Text == "")
+            {
+                size = 10;
+            }
+            else
+            {
+                size = Convert.ToInt32(comboBox1.Text);
+            }
+
+            Color newColor = ColorTranslator.FromHtml(color);
+            p = new Pen(newColor, size);
+            p.SetLineCap(System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.DashCap.Round);
+        }
+
+        private void pb_canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draw)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    end = e.Location;
+#pragma warning disable CA1416
+                    graphics.DrawLine(p, start, end);
+#pragma warning restore CA1416
+                    start = end;
+                    pb_canvas.Invalidate();
+                }
+            }
+        }
+
+        private void pb_canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            draw = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                color = "#" + (cd.Color.ToArgb() & 0x00FFFFFF).ToString("X6");
+                txt_color.Text = color;
+            }
+        }
+
+        private void save()
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.Filter = "PNG | .png";
+            if (s.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (File.Exists(s.FileName))
+                {
+                    File.Delete(s.FileName);
+                }
+                if (s.FileName.Contains(".png"))
+                {
+                    bmp.Save(s.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                }
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            save();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreateCanvas();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Drawing app built with C# via Visual Studio 2022\n" +
+                "Developped by PouletEnSlip © 2022 - All Rights Reserved\n" +
+                "GitHub : https://github.com/PouletEnSlip/Drawing", "About");
+        }
+    }
+}
